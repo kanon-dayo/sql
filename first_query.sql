1fbfc10b2245da5fce0789fa44d70b98868bbcfb
kanon-dayo <12kanon07@gmail.com>
2026-02-06 18:02:14 +0900

    20260206

--- first_query.sql
+++ first_query.sql
@@ -1,1 +1,30 @@
-SELECT. 'Postagres is ready' AS status;
+##SELECT statement
+SELECT 'Postagres is ready' AS status;
+SELECT * FROM actor;
+SELECT first_name FROM actor;
+SELECT first_name, last_name FROM actor;
+SELECT DISTINCT (release_year) FROM film;
+SELECT COUNT(rating) FROM film;
+SELECT COUNT(DISTINCT (title)) FROM film;
+SELECT COUNT(*) FROM payment;
+SELECT * FROM customer
+WHERE first_name = 'Jared';
+SELECT * FROM film
+WHERE rental_rate > 4 AND replacement_cost >= 19.99;
+SELECT * FROM film
+WHERE rental_rate > 4 AND replacement_cost >= 19.99
+AND rating='R';
+SELECT COUNT(title) FROM film
+WHERE rental_rate > 4 AND replacement_cost >= 19.99
+AND rating='R';
+SELECT * FROM film
+WHERE rating != 'R';
+SELECT * FROM customer
+WHERE first_name = 'Nancy' AND last_name = 'Thomas';
+#modified
+SELECT email FROM customer
+WHERE first_name = 'Nancy' AND last_name = 'Thomas';
+SELECT description FROM film
+WHERE title = 'Outlaw Hanky';
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
